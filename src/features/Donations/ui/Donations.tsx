5ce3338965dd1f8ab98548b7cb30cea10ed69f4a
Perevyazko <a.perevyazko@gmail.com>
2023-07-22 14:32:46 +0300

    add style mobile for widget donations

--- src/features/Donations/ui/Donations.tsx
+++ src/features/Donations/ui/Donations.tsx
@@ -7,6 +7,7 @@ import dog from "../../../shared/assets/images/Mops.png"
 import paw from "../../../shared/assets/icons/PawLogo.svg"
 import visa from "../../../shared/assets/images/Visa.png"
 import backVisa from "../../../shared/assets/images/BackVisa.png"
+import backVisaMobile from "../../../shared/assets/images/BackVisaMobile.png"
 import bone from "../../../shared/assets/icons/BigBone.svg"
 import {Input} from "../../../shared/ui/Input/Input";
 
@@ -38,22 +39,24 @@ export const DetailsDonations = memo((props: DonationsProps) => {
                 <img src={bone} className={cls.MiniBone}/>
                 <p className={cls.FirstParagraph}>Вместе мы можем сделать жизнь подопечных приюта лучше.</p>
                 <p className={cls.SecondParagraph}>Выберите сумму пожертвования</p>
-                <span className={cls.SelectDonations}>
-                    <Button className={cls.ButtonDonat}>300₽</Button>
-                    <Button className={cls.ButtonDonat}>500₽</Button>
-                    <Button className={cls.ButtonDonat}>1000₽</Button>
-                    <Button className={cls.ButtonDonat}>Другая сумма</Button>
-                    <Button className={cls.ButtonDonat}>Пожертвовать</Button>
-                </span>
-                <img src={backVisa} className={cls.BackVisa}/>
-                <img src={visa} className={cls.Visa}/>
-                <img src={paw} className={cls.SecondPaw}/>
+                <div className={cls.buttonGroup}>
+                    <Button className={cls.ButtonSumm}>300₽</Button>
+                    <Button className={cls.ButtonSumm}>500₽</Button>
+                    <Button className={cls.ButtonSumm}>1000₽</Button>
+                    <Button className={cls.ButtonSumm}>Другая сумма</Button>
+                </div>
+                <Button className={cls.ButtonDonat}>Пожертвовать</Button>
+                <div className={cls.BlockVisa}>
+                    <img src={backVisa} className={cls.BackVisa}/>
+                    <img src={backVisaMobile} className={cls.BackVisaMobile}/>
+                    <img src={visa} className={cls.Visa}/>
+                    <img src={paw} className={cls.SecondPaw}/>
+                    <Input className={cls.NumberCard} type={"number"} placeholder={"0000 0000 0000 0000"}/>
+                    <Input className={cls.MonthCard} type={"number"} placeholder={"MM"}/>
+                    <Input className={cls.YearCard} type={"number"} placeholder={"ГГ"}/>
+                    <Input className={cls.CvcCard} type={"number"} placeholder={"000"}/>
+                </div>
                 <img src={dog} className={cls.Dog}/>
-                <Input className={cls.NumberCard} type={"number"} placeholder={"0000 0000 0000 0000"}/>
-                <Input className={cls.MonthCard} type={"number"} placeholder={"MM"}/>
-                <Input className={cls.YearCard} type={"number"} placeholder={"ГГ"}/>
-                <Input className={cls.CvcCard} type={"number"} placeholder={"000"}/>
-
             </div>
         </ContentWrapper>
     );
