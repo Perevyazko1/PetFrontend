c81ed67d36cb5f559f1aeaedfd48d446676452ec
Perevyazko <a.perevyazko@gmail.com>
2023-10-05 14:12:09 +0300

    Refactoring Filter

--- src/features/Filter/Filter.tsx
+++ src/features/Filter/Filter.tsx
@@ -7,6 +7,7 @@ import chevronUp from "../../shared/assets/icons/chevronUp.svg"
 import {InputCheckbox} from "../../shared/ui/InputCheckbox/InputCheckbox";
 import {InputRadio} from "../../shared/ui/InputRadio/InputRadio";
 import {GroupRadio} from "../../shared/ui/GroupRadio/GroupRadio";
+import {HideBlockFilter} from "../../shared/ui/HideBlockFilter/HideBlockFilter";
 
 interface FilterProps {
     className?: string
@@ -20,116 +21,6 @@ export const Filter = memo((props: FilterProps) => {
     const [arrayBreed ,SetArrayBreed] = useState(["Терьер","Мэйнкун"])
     const [arrayColor ,SetArrayColor] = useState(["Белый","Палевый","Рыжий","Светлый","Тигровый","Темный","Все"])
     const [arraySize ,SetArraySize] = useState(["Крупный","Маленький","Небольшой","Средний","Все"])
-
-    const [dateReceipt, SetDateReceipt] = useState(false)
-    const [iconDateReceipt, SetIconDateReceipt] = useState(chevronDown)
-
-    const [summViews, SetSummViews] = useState(false)
-    const [iconSummViews, SetIconSummViews] = useState(chevronDown)
-
-    const [age, SetAge] = useState(false)
-    const [iconAge, SetIconAge] = useState(chevronDown)
-
-    const [typeAnimal, SetTypeAnimal] = useState(false)
-    const [iconTypeAnimal, SetIconTypeAnimal] = useState(chevronDown)
-
-    const [breed, SetBreed] = useState(false)
-    const [iconBreed, SetIconBreed] = useState(chevronDown)
-
-    const [color, SetColor] = useState(false)
-    const [iconColor, SetIconColor] = useState(chevronDown)
-
-    const [size, SetSize] = useState(false)
-    const [iconSize, SetIconSize] = useState(chevronDown)
-
-
-        const handleDateReceipt =()=>{
-        if(iconDateReceipt===chevronDown){
-
-           SetDateReceipt(true)
-            SetIconDateReceipt(chevronUp)
-        }else {
-            SetDateReceipt(false)
-            SetIconDateReceipt(chevronDown)
-        }
-
-    }
-
-
-        const handleSummViews =()=>{
-        if(iconSummViews===chevronDown){
-
-           SetSummViews(true)
-            SetIconSummViews(chevronUp)
-        }else {
-            SetSummViews(false)
-            SetIconSummViews(chevronDown)
-        }
-
-    }
-
-        const handleAge =()=>{
-            if(iconAge===chevronDown){
-
-               SetAge(true)
-                SetIconAge(chevronUp)
-            }else {
-                SetAge(false)
-                SetIconAge(chevronDown)
-            }
-
-        }
-
-        const handleTypeAnimal =()=>{
-            if(iconTypeAnimal===chevronDown){
-
-               SetTypeAnimal(true)
-                SetIconTypeAnimal(chevronUp)
-            }else {
-                SetTypeAnimal(false)
-                SetIconTypeAnimal(chevronDown)
-            }
-
-        }
-
-        const handleBreed =()=>{
-            if(iconBreed===chevronDown){
-
-               SetBreed(true)
-                SetIconBreed(chevronUp)
-            }else {
-                SetBreed(false)
-                SetIconBreed(chevronDown)
-            }
-
-        }
-
-        const handleColor =()=>{
-            if(iconColor===chevronDown){
-
-               SetColor(true)
-                SetIconColor(chevronUp)
-            }else {
-                SetColor(false)
-                SetIconColor(chevronDown)
-            }
-
-        }
-
-
-        const handleSize =()=>{
-            if(iconSize===chevronDown){
-
-               SetSize(true)
-                SetIconSize(chevronUp)
-            }else {
-                SetSize(false)
-                SetIconSize(chevronDown)
-            }
-
-        }
-
-
 
     const {
         className,
@@ -149,48 +40,14 @@ export const Filter = memo((props: FilterProps) => {
             <p className={cls.HeaderFilters}>Искать в содержимом статьи:</p>
             <Input className={cls.Input}/>
             <div className={cls.TypeFilter}>Сортировать по:</div>
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Дата приема</p>
-                <img onClick={handleDateReceipt} src={iconDateReceipt}/>
-            </div>
-            {dateReceipt &&
+            <HideBlockFilter nameBlock={"Дата приема"}>
                 <GroupRadio decrease={"dateReceiptDecrease"} increase={"dateReceiptIncrease"}/>
-                // <div>
-                //     <div className={cls.CheckHeader}>
-                //         <InputRadio onChange={event =>{ console.log(event.target.checked)}}/>
-                //         <p>По возрастанию</p>
-                //     </div>
-                //     <div className={cls.CheckHeader}>
-                //        <InputRadio/>
-                //         <p>По убыванию</p>
-                //     </div>
-                // </div>
-            }
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Количество просмотров </p>
-                <img onClick={handleSummViews} src={iconSummViews}/>
-            </div>
-            {summViews &&
+            </HideBlockFilter>
+            <HideBlockFilter nameBlock={"Количество просмотров"}>
                 <GroupRadio decrease={"summViewsPostPetDecrease"} increase={"summViewsPostPetIncrease"}/>
-               // <div>
-               //      <div className={cls.CheckHeader}>
-               //          <InputRadio/>
-               //          <p>По возрастанию</p>
-               //      </div>
-               //      <div className={cls.CheckHeader}>
-               //         <InputRadio/>
-               //          <p>По убыванию</p>
-               //      </div>
-               //  </div>
-            }
-
+            </HideBlockFilter>
             <div className={cls.TypeFilter}>Отфлиртовать по:</div>
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Возрасту</p>
-                <img onClick={handleAge} src={iconAge}/>
-            </div>
-            {age &&
-            <div>
+            <HideBlockFilter nameBlock={"Возрасту"}>
                 <div className={cls.BlockInput}>
                     <p>От</p>
                     <Input className={cls.UnitInput} placeholder={"   — —"} type={"number"}/>
@@ -198,40 +55,16 @@ export const Filter = memo((props: FilterProps) => {
                     <Input className={cls.UnitInput} placeholder={"   — —"} type={"number"}/>
                 </div>
                 <GroupRadio decrease={"AgePetDecrease"} increase={"AgePetIncrease"}/>
-                {/*<div className={cls.CheckHeader}>*/}
-                {/*    <InputRadio/>*/}
-                {/*    <p>По возрастанию</p>*/}
-                {/*</div>*/}
-                {/*<div className={cls.CheckHeader}>*/}
-                {/*   <InputRadio/>*/}
-                {/*    <p>По убыванию</p>*/}
-                {/*</div>*/}
-            </div>
-            }
-
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Вид животного</p>
-                <img onClick={handleTypeAnimal} src={iconTypeAnimal}/>
-            </div>
-            {typeAnimal &&
-                <div>
+            </HideBlockFilter>
+            <HideBlockFilter nameBlock={"Вид животного"}>
                     {arrayTypePet.map(type=>(
                    <div key={type} className={cls.CheckHeader}>
                         <InputCheckbox/>
                         <p>{type}</p>
                     </div>
-
                     ))}
-                </div>
-            }
-
-
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Порода</p>
-                <img src={iconBreed} onClick={handleBreed}/>
-            </div>
-            {breed &&
-                <div>
+            </HideBlockFilter>
+            <HideBlockFilter nameBlock={"Порода"}>
                     {arrayBreed.map(breedPet=>(
                    <div key={breedPet} className={cls.CheckHeader}>
                         <InputCheckbox/>
@@ -239,38 +72,23 @@ export const Filter = memo((props: FilterProps) => {
                     </div>
 
                     ))}
-                </div>
-
-            }
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters}>Окрас</p>
-                <img src={iconColor} onClick={handleColor}/>
-            </div>
-            {color &&
-                <div>
+            </HideBlockFilter>
+            <HideBlockFilter nameBlock={"Окрас"}>
                     {arrayColor.map(colorAnimal =>(
                         <div key={colorAnimal} className={cls.CheckHeader}>
                             <InputCheckbox/>
                             <p>{colorAnimal}</p>
                         </div>
                     ))}
-                </div>
-            }
-            <div className={cls.CheckHeader}>
-                <p className={cls.HeaderFilters} >Размер</p>
-                <img src={iconSize} onClick={handleSize}/>
-            </div>
-            {size &&
-                <div>
+            </HideBlockFilter>
+            <HideBlockFilter nameBlock={"Размер"}>
                     {arraySize.map(sizeAnimal=>(
                         <div key={sizeAnimal} className={cls.CheckHeader}>
                             <InputCheckbox/>
                             <p>{sizeAnimal}</p>
                         </div>
-
                     ))}
-                </div>
-            }
+            </HideBlockFilter>
         </div>
     );
 });
---
+++ src/shared/ui/HideBlockFilter/HideBlockFilter.tsx
@@ -0,0 +1,60 @@
+import {memo, ReactNode, useState} from 'react';
+import {classNames, Mods} from "shared/lib/classNames/classNames";
+import cls from "./HideBlockFilter.module.scss"
+import chevronDown from "../../assets/icons/ChevronDown.svg";
+import chevronUp from "../../assets/icons/chevronUp.svg";
+
+
+interface HideBlockFilterProps {
+    className?: string
+    children?: ReactNode
+    nameBlock: string
+}
+
+
+export const HideBlockFilter = memo((props: HideBlockFilterProps) => {
+    const {
+        className,
+        children,
+        nameBlock,
+        ...otherProps
+    } = props
+
+    const [hideBlock, SetHideBlock] = useState(false)
+    const [icon, SetIcon] = useState(chevronDown)
+
+            const handleSummViews =()=>{
+        if(icon===chevronDown){
+
+           SetHideBlock(true)
+            SetIcon(chevronUp)
+        }else {
+            SetHideBlock(false)
+            SetIcon(chevronDown)
+        }
+
+    }
+
+
+
+    const mods: Mods = {
+
+    };
+
+    return (
+        <div
+            className={classNames(cls.HideBlockFilter, mods, [className])}
+            {...otherProps}
+        >
+            <div className={cls.CheckHeader}>
+                <p className={cls.HeaderFilters}>{nameBlock}</p>
+                <img onClick={handleSummViews} src={icon}/>
+            </div>
+            {hideBlock &&
+                <>
+                    {children}
+                </>
+            }
+        </div>
+    );
+});
